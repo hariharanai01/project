4ccb14a91cbb1e6f0617c5e42f81e35c90758fd1
hariharananbuai01 <hariharananbuai01@gmail.com>
2024-01-22 13:33:29 +0530

    added files

--- Cplus/accessspecifierdemo/main.cpp
+++ Cplus/accessspecifierdemo/main.cpp
@@ -5,7 +5,44 @@ class productcount
 {
 
 private:
-    int product;
+    int productprice;
 
 public:
-    string
+
+    productcount()
+    {
+
+        cout << "Enter the product name";
+        string name;
+        cin >> name;
+        cout << "this is my product" << name;
+    }
+    void setproductprice(int a)
+    {
+        productprice=a;
+
+    }
+    int getproductprice()
+    {
+        return productprice;
+    }
+    int getproductprice01()
+    {
+        cout << productprice;
+    }
+    void setproductprice02()
+    {
+        cout << productprice;
+    }
+};
+
+int main()
+{
+    productcount pc;
+    pc.setproductprice(500);
+    pc.getproductprice();
+    cout << pc.getproductprice() << endl;
+    pc.getproductprice01();
+    pc.setproductprice02();
+    return 0;
+}
